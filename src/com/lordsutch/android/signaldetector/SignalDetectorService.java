ee6a1151ef04ca654ef30d42694283bec594afc0
Chris Lawrence <lordsutch@gmail.com>
2013-07-12 03:53:28 -0400

    Simplify location code to just use best provider.

--- src/com/lordsutch/android/signaldetector/SignalDetectorService.java
+++ src/com/lordsutch/android/signaldetector/SignalDetectorService.java
@@ -99,18 +99,14 @@ public class SignalDetectorService extends Service {
     	gpsCriteria.setCostAllowed(false);
     	gpsCriteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
     	
-    	List<String> providers = mLocationManager.getProviders(gpsCriteria, true);
-    	
-    	for(String provider : providers) {
-    		Log.d(TAG, "Registering "+provider);
-    		mLocationManager.requestLocationUpdates(provider, 500, 0, mLocListener);
-    		Location mLoc = mLocationManager.getLastKnownLocation(provider);
-
-    		if(mLoc != null) {
-        		updateLocations(mLoc);
-    			mLocation = getBetterLocation(mLoc, mLocation);
-    		}
-    	}
+    	String provider = mLocationManager.getBestProvider(gpsCriteria, true);    	
+    	Log.d(TAG, "Registering "+provider);
+		mLocationManager.requestLocationUpdates(provider, 1000, (float) 0, mLocListener);
+		Location mLoc = mLocationManager.getLastKnownLocation(provider);    	
+		if(mLoc != null) {
+    		updateLocations(mLoc);
+			mLocation = mLoc;
+		}
     	mNotifyMgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
     	
     	return mBinder;
@@ -497,10 +493,10 @@ public class SignalDetectorService extends Service {
     	public void onLocationChanged(Location mLoc)
     	{
         	updateLocations(mLoc);
-        	
-    		mLocation = getBetterLocation(mLoc, mLocation);
-
-    		updatelog(true);
+        	if(mLocation != mLoc) {
+        		mLocation = mLoc;
+        		updatelog(true);
+        	}
     	}
 
 		@Override
@@ -521,68 +517,7 @@ public class SignalDetectorService extends Service {
 			
 		}
     };
-
-    private static final int TWO_MINUTES = 1000 * 60 * 2;
-
-    /** Determines whether one Location reading is better than the current Location fix.
-     * Code taken from
-     * http://developer.android.com/guide/topics/location/obtaining-user-location.html
-     *
-     * @param newLocation  The new Location that you want to evaluate
-     * @param currentBestLocation  The current Location fix, to which you want to compare the new
-     *        one
-     * @return The better Location object based on recency and accuracy.
-     */
-   protected Location getBetterLocation(Location newLocation, Location currentBestLocation) {
-       if (currentBestLocation == null) {
-           // A new location is always better than no location
-           return newLocation;
-       }
-
-       // Check whether the new location fix is newer or older
-       long timeDelta = newLocation.getTime() - currentBestLocation.getTime();
-       boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
-       boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
-       boolean isNewer = timeDelta > 0;
-
-       // If it's been more than two minutes since the current location, use the new location
-       // because the user has likely moved.
-       if (isSignificantlyNewer) {
-           return newLocation;
-       // If the new location is more than two minutes older, it must be worse
-       } else if (isSignificantlyOlder) {
-           return currentBestLocation;
-       }
-
-       // Check whether the new location fix is more or less accurate
-       int accuracyDelta = (int) (newLocation.getAccuracy() - currentBestLocation.getAccuracy());
-       boolean isLessAccurate = accuracyDelta > 0;
-       boolean isMoreAccurate = accuracyDelta < 0;
-       boolean isSignificantlyLessAccurate = accuracyDelta > 200;
-
-       // Check if the old and new location are from the same provider
-       boolean isFromSameProvider = isSameProvider(newLocation.getProvider(),
-               currentBestLocation.getProvider());
-
-       // Determine location quality using a combination of timeliness and accuracy
-       if (isMoreAccurate) {
-           return newLocation;
-       } else if (isNewer && !isLessAccurate) {
-           return newLocation;
-       } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
-           return newLocation;
-       }
-       return currentBestLocation;
-   }
-
-   /** Checks whether two providers are the same */
-   private boolean isSameProvider(String provider1, String provider2) {
-       if (provider1 == null) {
-         return provider2 == null;
-       }
-       return provider1.equals(provider2);
-   }
-
+    
     // Listener for signal strength.
     final PhoneStateListener mListener = new PhoneStateListener()
     {
@@ -611,9 +546,9 @@ public class SignalDetectorService extends Service {
     	public void onSignalStrengthsChanged(SignalStrength sStrength)
     	{
     		mSignalStrength = sStrength;
-//    		if(mSignalStrength != null) {
-//    			Log.d(TAG, mSignalStrength.toString());
-//    		}
+    		if(mSignalStrength != null) {
+    			Log.d(TAG, mSignalStrength.toString());
+    		}
     		updatelog(true);
     	}
     };
