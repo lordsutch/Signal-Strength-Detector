906539ae5c62e06b469def2ce7ed388c5faaf5a8
Chris Lawrence <lordsutch@gmail.com>
2017-08-30 23:45:15 -0400

    Simplify cache clearing code.

--- src/com/lordsutch/android/signaldetector/SettingsFragment.java
+++ src/com/lordsutch/android/signaldetector/SettingsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
+import android.webkit.WebView;
 import android.widget.Toast;
 
 public class SettingsFragment extends PreferenceFragment {
@@ -19,14 +20,14 @@ public class SettingsFragment extends PreferenceFragment {
         myPref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                if (SignalDetector.leafletView != null) {
-                    SignalDetector.leafletView.clearCache(true);
-                    Context context = getActivity();
-                    CharSequence text = context.getString(R.string.map_tile_cache_cleared);
-                    int duration = Toast.LENGTH_SHORT;
+                Context context = getActivity();
+                CharSequence text = context.getString(R.string.map_tile_cache_cleared);
+                int duration = Toast.LENGTH_SHORT;
 
-                    Toast.makeText(context, text, duration).show();
-                }
+                WebView webView = new WebView(context);
+                webView.clearCache(true);
+
+                Toast.makeText(context, text, duration).show();
                 return false;
             }
         });
